66ba2b536f9a22af7db073b56d9976722d5a0f8c
Alex <alexander_kurihara@brown.edu>
2013-12-08 00:24:34 -0500

    Oauth redirect working at /login

--- routes/index.js
+++ routes/index.js
@@ -8,6 +8,33 @@ exports.index = function(req, res){
 };
 
 exports.main = function(req, res){
-  console.log("Inside this function");
   res.render('main');
+};
+
+exports.login = function(req, res){
+	var OAuth = require('OAuth');
+    var oauth = new OAuth.OAuth(
+      "http://api.rdio.com/oauth/request_token",
+      "http://api.rdio.com/oauth/access_token",
+      "62nph9uusuqtqt3vdykqr2bx",
+      "KVgu9dkymP",
+      "1.0",
+      "http://localhost:3000/main",
+      "HMAC-SHA1"
+    );
+
+	oauth.getOAuthRequestToken(function(error, oauth_token, oauth_token_secret, results){
+		console.log(oauth_token);
+		console.log(oauth_token_secret);
+		console.log(results.login_url); 
+
+		/*
+		req.session.oa = oauth;
+		req.session.oauth_token = oauth_token;
+		req.session.oauth_token_secret = oauth_token_secret;
+		*/
+	                
+		// redirect the user to authorize the token
+		res.redirect(results.login_url + '?oauth_token=' + oauth_token);              
+	});
 };
