06a9c953aa04f6cf01d7b685a736de1271e92edc
Alex <alexander_kurihara@brown.edu>
2013-12-08 00:45:35 -0500

    oauth part 2

--- app.js
+++ app.js
@@ -8,6 +8,7 @@ var routes = require('./routes');
 var http = require('http');
 var path = require('path');
 var socketio = require('socket.io')
+var OAuth = require('OAuth');
 var app = express();
 
 //require('./db');
@@ -21,6 +22,8 @@ app.use(express.favicon());
 app.use(express.logger('dev'));
 app.use(express.bodyParser());
 app.use(express.methodOverride());
+app.use(express.cookieParser());
+app.use(express.session({ secret: 'test' }));
 app.use(app.router);
 app.use(express.static(path.join(__dirname, 'public')));
 //app.use(express.static(__dirname, '/public'));
@@ -32,6 +35,22 @@ if ('development' == app.get('env')) {
 
 app.get('/', routes.index);
 app.get('/main', routes.main);
+app.get('/login', routes.login);
+
+/*
+oauth.post(
+    "http://api.rdio.com/1/",
+    null,
+    null,
+    {
+        method: "get",
+        keys: "a184236,a254895,a242205"
+    },
+    function(result) {
+        console.log(result);
+    }
+);
+*/
 
 var server = app.listen(app.get('port'), function(){
   console.log('Express server listening on port ' + app.get('port'));
--- routes/index.js
+++ routes/index.js
@@ -8,31 +8,48 @@ exports.index = function(req, res){
 };
 
 exports.main = function(req, res){
+	var OAuth = require('OAuth');
+	var oauth = new OAuth.OAuth(
+		  "http://api.rdio.com/oauth/request_token",
+		  "http://api.rdio.com/oauth/access_token",
+		  "62nph9uusuqtqt3vdykqr2bx",
+		  "KVgu9dkymP",
+		  "1.0",
+		  "http://localhost:3000/main",
+		  "HMAC-SHA1"
+	);
+
+	oauth.getOAuthAccessToken(
+		req.session.oauth_token, 
+    req.session.oauth_token_secret,
+    req.param('oauth_verifier'),
+    function(error, oauth_access_token, oauth_access_token_secret, results2) {
+    	req.session.oauth_token = oauth_token;
+			req.session.oauth_token_secret = oauth_token_secret;
+    });
   res.render('main');
 };
 
 exports.login = function(req, res){
 	var OAuth = require('OAuth');
-    var oauth = new OAuth.OAuth(
-      "http://api.rdio.com/oauth/request_token",
-      "http://api.rdio.com/oauth/access_token",
-      "62nph9uusuqtqt3vdykqr2bx",
-      "KVgu9dkymP",
-      "1.0",
-      "http://localhost:3000/main",
-      "HMAC-SHA1"
-    );
+	var oauth = new OAuth.OAuth(
+	  "http://api.rdio.com/oauth/request_token",
+	  "http://api.rdio.com/oauth/access_token",
+	  "62nph9uusuqtqt3vdykqr2bx",
+	  "KVgu9dkymP",
+	  "1.0",
+	  "http://localhost:3000/main",
+	  "HMAC-SHA1"
+	);
 
 	oauth.getOAuthRequestToken(function(error, oauth_token, oauth_token_secret, results){
-		console.log(oauth_token);
-		console.log(oauth_token_secret);
-		console.log(results.login_url); 
+		//console.log(oauth_token);
+		//console.log(oauth_token_secret);
+		//console.log(results.login_url); 
 
-		/*
 		req.session.oa = oauth;
 		req.session.oauth_token = oauth_token;
 		req.session.oauth_token_secret = oauth_token_secret;
-		*/
 	                
 		// redirect the user to authorize the token
 		res.redirect(results.login_url + '?oauth_token=' + oauth_token);              
