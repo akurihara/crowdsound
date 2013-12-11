f7c6049dd99fedac26fd61b3433ddebf5002e3d2
kojiyama <kojiluke@gmail.com>
2013-12-11 02:52:01 -0500

    playlist liking

--- public/js/playlist.js
+++ public/js/playlist.js
@@ -1,6 +1,7 @@
 var initPlaylist = function(playlist) {
     console.log("initialized playlist");
 
+	// populate playlist with songs
     $(".playlist").empty();
     for (var i=0; i<playlist.length; i++) {
         var item = playlist[i];
@@ -20,12 +21,18 @@ var initPlaylist = function(playlist) {
                 playlist_item_like: like,
                 playlist_item_key: item.key
             });
-    		
-
-
-    
     };
+    // attach click events to songs in list
+    $(".likeable").each(function() {
+        $(this).unbind();
+        $(this).click(function() {
+        	var trackKey = $(this).attr("trackkey");
+            console.log(trackKey);
+            postUpvote(trackKey);
+        });
+    });
    
+    // update playlist
 	socket.on('playlist', function(object) {
 		if (m_currentSong == null && object.playlist[0] != null) {
   		   m_currentSong = {};
@@ -46,60 +53,48 @@ var initPlaylist = function(playlist) {
 
 		    $('.seek').attr({'max': m_currentSong.duration});
 
-		    // busy loop until apiswf loaded
+		   // busy loop until apiswf loaded
            while (apiswf == null) {}
 
            getRemovePlayed();
            apiswf.rdio_play(object.playlist[0].key);
            m_currentSong.isPlaying = true;
            playBtn.removeClass('glyphicon-play').addClass('glyphicon-pause');
-  		}
+  		} // if current song 
 
 	    $(".playlist").empty();
+	    
+		// populate playlist with songs
 	    for (var i=0; i<object.playlist.length; i++) {
 	        var item = object.playlist[i];
 	        var d = "<div id='playlist_item_"+i+"'></div>";
 	        $(".playlist").append(d);
 	
 	        var heartState = item.like ? "glyphicon-heart" : "glyphicon-heart-empty";
-	        var like = "<span class='glyphicon " + heartState + "'></span>"; 
+	        var like = "<span trackkey='" + item.key + "' class='likeable glyphicon " + heartState + "'></span>"; 
 	
 	        $("#playlist_item_"+i).loadTemplate($("#playlist_item"),
-	            {
-	                playlist_item_index: i+1,
-	                playlist_item_song_name: item.name,
-	                playlist_item_artist: item.artist,
-	                playlist_item_album: item.album,
-	                playlist_item_rating: item.rating,
-	                playlist_item_like: like,
-	                playlist_item_key: item.key
-	            });
-	    };
-	});
-	/*
-	function() {
-		getRemovePlayed();
-		playlist.
-		$('.now_playing_song').html(m_currentSong.songName);
-    	$('.now_playing_artist').html(m_currentSong.artist);
-    	$('.now_playing_album').html(m_currentSong.album);
-    	$('.progress').attr("max", m_currentSong.duration);
-    	
-	}
-	*/
-	/*
-	 $(".likeable").each(function() {
-            $(this).unbind();
-            $(this).click(function() {
-            	//item.like = true;
-            	
-                var songName= $(this).attr("songName");
-               	
-                postUpvote(trackKey);
+            {
+                playlist_item_index: i+1,
+                playlist_item_song_name: item.song_name,
+                playlist_item_artist: item.artist,
+                playlist_item_album: item.album,
+                playlist_item_rating: item.rating,
+                playlist_item_like: like,
+                playlist_item_key: item.key
             });
-     });
-	*/
-	
-};
+	    }; // for loop
+	    
+	    
+        $(".likeable").each(function() {
+        	$(this).unbind();
+        	$(this).click(function() {
+           		var trackKey = $(this).attr("trackkey");
+                console.log(trackKey);
+                postUpvote(trackKey);
+        	});
+    	}); //likeable each    
+	});	//socket
+};  //init playlist
 
 
